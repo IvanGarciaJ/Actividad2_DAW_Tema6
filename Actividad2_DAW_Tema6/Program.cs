5ced7657fe487d4772768286cca07ebacd8385f5
IvanGarciaJ <115452796+IvanGarciaJ@users.noreply.github.com>
2022-11-23 11:44:42 +0100

    Add files via upload

---
+++ Actividad2_DAW_Tema6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Actividad2_DAW_Tema6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prueba");
+        }
+    }
+}
